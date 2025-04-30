1dfbba020980e96564ab760a525e7ab08d6af585
Felipe Sorrentino <161666209+Felipssv@users.noreply.github.com>
2025-04-29 21:37:28 -0300

    Update Program.java

--- UdemyJava/Section_13/Heranca/Ex1/src/application/Program.java
+++ UdemyJava/Section_13/Heranca/Ex1/src/application/Program.java
@@ -12,12 +12,29 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Account acc = new Account(1001, "Alex", 0.0);
+
+        Account acc1 = new Account(1001, "Alex", 1000.0);
+        acc1.withdraw(200.0);
+        System.out.println(acc1.getBalance());
+
+        Account acc2 = new SavingsAccount(1002, "jorge", 1000.0, 0.01);
+        acc2.withdraw(200.0);
+        System.out.println(acc2.getBalance());
+
+        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
+        
+
+        /*
+        Account acc = new Account(1200, "Jorge", 1235.0);
+       
         BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
 
         //UPCASTING
 
-        Account acc1 = bacc;
+        acc1 = bacc;
         Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
         Account acc3 = new SavingsAccount(1004, "Anna ", 0.0, 0.01);
 
@@ -38,10 +55,8 @@ public class Program {
             acc5.UpdateBalance();
             System.out.println("Update!");
         }
-        
-
-
-
+             System.out.println(acc.getBalance());
+         */
         sc.close();
     }
 }
